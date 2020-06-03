94f18e71fb988307540431707e8918790dd8e52f
pp <pp@localhost>
2020-06-03 16:08:12 +0200

    initial

---
+++ tc.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+static const char PRE[] = "\x1b(0"; // Prefix, to enable special "Drawing Characters"
+static const char SUF[] = "\x1b(B"; // Disable given special "Drawing Characters"
+// list of drawing characters from the vt100 spec
+static const char BOTTOM_RIGHT[2] = {0x6a};
+static const char TOP_RIGHT[2] = {0x6b};
+static const char TOP_LEFT[2] = {0x6c};
+static const char BOTTOM_LEFT[2] = {0x6d};
+static const char CROSSING[2] = {0x6e};
+static const char HORIZ_LINE[2] = {0x71};
+static const char T_RIGHT[2] = {0x74};
+static const char T_LEFT[2] = {0x75};
+static const char T_UP[2] = {0x76};
+static const char T_DOWN[2] = {0x77};
+static const char VERT_LINE[2] = {0x78};
+
+
+#define VT100_COMPAT_MODE 0
+#define clear() printf("\033[H\033[J");
+#define locate(x, y) printf("\033[%d;%dH", (y), (x));
+#define plot_vt100_char(c) printf("%s%s%s", PRE, (c), SUF);
+
+struct winsize get_terminal_size()
+{
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	return w;
+}
+
+void plot_outer_border(int lines, int columns)
+{
+	locate(1, 1);
+	plot_vt100_char(TOP_LEFT);
+	for (int i = 2; i < columns; i++)
+	{
+		locate(i, 0);
+		plot_vt100_char(HORIZ_LINE);
+	}
+	plot_vt100_char(TOP_RIGHT);
+	for (int i = 2; i < lines-1; i++)
+	{
+		locate(1, i);
+		plot_vt100_char(VERT_LINE);
+		locate(columns, i);
+		plot_vt100_char(VERT_LINE);
+	}
+	locate(0, lines-1);
+	plot_vt100_char(BOTTOM_LEFT);
+	locate(columns, lines-1);
+	plot_vt100_char(BOTTOM_RIGHT);
+}
+
+
+int main(int argc, char **argv)
+{
+	struct winsize w = get_terminal_size();
+	if (w.ws_row < 24 || w.ws_col < 80)
+	{
+		printf("Terminal Height/Width must be greater than 80x24\n");
+		printf("But yours is only %ix%i\n", w.ws_col, w.ws_row);
+		return 1;
+	}
+	clear();
+	plot_outer_border(w.ws_row, w.ws_col);	
+	return 0;
+}
